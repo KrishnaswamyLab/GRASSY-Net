a6ac9d5006062c46c6335593550e12b44be2dc83
Shaul Tolkowsky <stolkowsky@gmail.com>
2026-01-13 19:14:58 +0200

    Fix numpy array truth value ambiguity in get_all_metrics
    Line 77: Changed 'train = train or get_dataset()' to explicit None check.
    The 'or' operator fails with numpy arrays due to ambiguous truth values.
    This fix allows passing custom train/test arrays to get_all_metrics().

--- external/moses/moses/metrics/metrics.py
+++ external/moses/moses/metrics/metrics.py
@@ -74,7 +74,8 @@ def get_all_metrics(gen, k=None, n_jobs=1,
         test_scaffolds = get_dataset('test_scaffolds')
         ptest_scaffolds = get_statistics('test_scaffolds')
 
-    train = train or get_dataset('train')
+    # Fix for numpy array truth value ambiguity
+    train = get_dataset('train') if train is None else train
 
     if k is None:
         k = [1000, 10000]
