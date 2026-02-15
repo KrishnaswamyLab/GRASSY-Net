5ebc4de094cb0a2f3893a376e8e169e61ebbbd65
shaulTol <stolkowsky@gmail.com>
2026-02-15 04:54:21 -0800

    add BBAB grokking ablation: wd=0.5 and wd=1.0 configs

---
+++ scripts/ZINC/BBAB/train_grassy_dit_h1024_wd10.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=BBAB_wd10
+#SBATCH --partition=catfish
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH --time=168:00:00
+#SBATCH --output=BBAB_wd10_%j.out
+#SBATCH --error=BBAB_wd10_%j.err
+
+source molenv/bin/activate
+cd /sci/labs/orzuk/shaulytolk/GRASSY-Net
+export XDG_CACHE_HOME=/tmp/cache_$SLURM_JOB_ID
+export PIP_NO_CACHE_DIR=1
+export WANDB_DIR=/sci/labs/orzuk/shaulytolk/GRASSY-Net/wandb
+export WANDB_CACHE_DIR=/tmp/wandb_cache_$SLURM_JOB_ID
+
+python -m grassy_dit.train --config configs/ZINC/BBAB/BBAB_dit_h1024_wd10.yaml
